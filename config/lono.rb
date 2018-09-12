a41722f4d91dae8644f8aeb59c12a81c8ca8d731
Tamino Martinius <dev@zaku.eu>
2018-09-12 22:40:37 +0200

    Add own/new vpc to permutations

--- config/lono.rb
+++ config/lono.rb
@@ -1,11 +1,12 @@
-def generate(visibility, zones, version)
-  template "#{version}-#{visibility}-#{zones}az.yaml" do
+def generate(visibility, zones, version, own_vpc)
+  template "#{version}-#{own_vpc ? 'own-vpc' : 'new-vpc'}-#{visibility}-#{zones}az.yaml" do
     source "sentry-formation.yaml.erb"
     variables(
-      :Description => "Sentry.io #{visibility} setup in #{zones} availability zones",
-      :visibility => visibility,
-      :availability_zones => zones,
-      :version => version
+      Description: "Sentry.io #{visibility} setup in #{zones} availability zones",
+      visibility: visibility,
+      availability_zones: zones,
+      version: version,
+      own_vpc: own_vpc
     )
   end
 end
@@ -16,8 +17,10 @@ end
 
 [get_version(), 'master'].each do |version|
   ["internal", "internet-facing"].each do |visibility|
-    (2..3).each do |zones|
-      generate(visibility, zones, version)
+    [true, false].each do |own_vpc|
+      (2..3).each do |zones|
+        generate(visibility, zones, version, own_vpc)
+      end
     end
   end
 end
